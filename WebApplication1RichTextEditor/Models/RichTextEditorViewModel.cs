a0c1c4349b92085cf573048c61e00a246f145574
stephenbek <stefanosbek@hotmail.com>
2019-07-09 18:45:07 +1000

    Add files via upload

---
+++ WebApplication1RichTextEditor/Models/RichTextEditorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1RichTextEditor.Models
+{
+    public class RichTextEditorViewModel
+    {
+        [AllowHtml]
+        [Display(Name = "Message")]
+        public string Message
+        {
+            get;
+            set;
+        }
+    }
+}
